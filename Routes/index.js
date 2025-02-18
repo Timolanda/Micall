e8f2978602d2aacb284f037f606f288d4ee1160b
Timolanda <timolanda@gmail.com>
2025-02-18 13:29:46 +0300

    Delete Routes directory

--- Routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = require('./app');  // Import app.js
-const PORT = process.env.PORT || 3000;
-
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
